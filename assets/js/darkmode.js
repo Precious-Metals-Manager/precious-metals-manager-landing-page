0aed4d570575398a7a320ef5988965304bb462ad
Sergio Martin Rubio <26189335+smartinrub@users.noreply.github.com>
2024-04-27 23:30:59 +0200

    Add dark screenshots

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -5,12 +5,22 @@ function setLightElements() {
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
     document.getElementById("appStoreDownload").src = "assets/appstore.svg";
+    document.getElementById("heroScreenshot").src = "assets/screenshot/screenshot1.webp";
+    document.getElementById("feature1Screenshot").src = "assets/screenshot/screenshot2.webp";
+    document.getElementById("feature2Screenshot").src = "assets/screenshot/screenshot3.webp";
+    document.getElementById("feature3Screenshot").src = "assets/screenshot/screenshot4.webp";
+    document.getElementById("feature4Screenshot").src = "assets/screenshot/screenshot5.webp";
 }
 
 function setDarkElements() {
     icon.classList.remove('fa-sun');
     icon.classList.add('fa-moon');
     document.getElementById("appStoreDownload").src = "assets/appstorewhite.svg";
+    document.getElementById("heroScreenshot").src = "assets/screenshot/screenshot1dark.webp";
+    document.getElementById("feature1Screenshot").src = "assets/screenshot/screenshot2dark.webp";
+    document.getElementById("feature2Screenshot").src = "assets/screenshot/screenshot3dark.webp";
+    document.getElementById("feature3Screenshot").src = "assets/screenshot/screenshot4dark.webp";
+    document.getElementById("feature4Screenshot").src = "assets/screenshot/screenshot5dark.webp";
 }
 
 function switchTheme(e) {
