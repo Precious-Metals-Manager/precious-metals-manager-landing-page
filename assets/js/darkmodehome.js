b2c0ec72917bbb5284d009c32697cf25ac33bb6d
Sergio Martin Rubio <26189335+smartinrub@users.noreply.github.com>
2024-05-01 21:21:06 +0200

    Use separate script for home

--- assets/js/darkmodehome.js
+++ assets/js/darkmodehome.js
@@ -1,6 +1,24 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const icon = document.querySelector('.darkModeIcon i');
 
+function setLightImages() {
+    document.getElementById("appStoreDownload").src = "assets/appstore.svg";
+    document.getElementById("heroScreenshot").src = "assets/screenshot/screenshot1.webp";
+    document.getElementById("feature1Screenshot").src = "assets/screenshot/screenshot2.webp";
+    document.getElementById("feature2Screenshot").src = "assets/screenshot/screenshot3.webp";
+    document.getElementById("feature3Screenshot").src = "assets/screenshot/screenshot4.webp";
+    document.getElementById("feature4Screenshot").src = "assets/screenshot/screenshot5.webp";
+}
+
+function setDarkImages() {
+    document.getElementById("appStoreDownload").src = "assets/appstorewhite.svg";
+    document.getElementById("heroScreenshot").src = "assets/screenshot/screenshot1dark.webp";
+    document.getElementById("feature1Screenshot").src = "assets/screenshot/screenshot2dark.webp";
+    document.getElementById("feature2Screenshot").src = "assets/screenshot/screenshot3dark.webp";
+    document.getElementById("feature3Screenshot").src = "assets/screenshot/screenshot4dark.webp";
+    document.getElementById("feature4Screenshot").src = "assets/screenshot/screenshot5dark.webp";
+}
+
 function setLightIcon() {
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
@@ -16,10 +34,12 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         setDarkIcon()
+        setDarkImages()
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
         setLightIcon()
+        setLightImages()
     }
 }
 
@@ -29,18 +49,22 @@ if (savedTheme) {
     if (savedTheme === 'dark') {
         toggleSwitch.checked = true
         setDarkIcon()
+        setDarkImages()
     } else {
         toggleSwitch.checked = false
         setLightIcon()
+        setLightImages()
     }
 } else {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.documentElement.setAttribute('data-theme', 'dark');
         toggleSwitch.checked = true
         setDarkIcon()
+        setDarkImages()
     } else {
         toggleSwitch.checked = false
         setLightIcon()
+        setLightImages()
     }
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
@@ -49,10 +73,12 @@ if (savedTheme) {
             document.documentElement.setAttribute('data-theme', 'dark');
             toggleSwitch.checked = true
             setDarkIcon()
+            setDarkImages()
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
             toggleSwitch.checked = false
             setLightIcon()
+            setLightImages()
         }
     });
 }
